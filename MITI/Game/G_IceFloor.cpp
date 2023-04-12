e568ca4a93e517833df16913044b04a075e1ffcb
ca01972014 <ca01972014@st.kawahara.ac.jp>
2023-04-12 12:35:18 +0900

    モデルの上げ忘れと氷床クラス

---
+++ MITI/Game/G_IceFloor.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "G_IceFloor.h"
+
+G_IceFloor::G_IceFloor()
+{
+
+}
+
+G_IceFloor::~G_IceFloor()
+{
+
+}
+
+void G_IceFloor::Update()
+{
+
+}
+
+void G_IceFloor::Render(RenderContext& rc)
+{
+
+}
