46363b833d2522db3538fb84da4d9fa84487ac4c
CA01972006 <CA01972006@st.kawahara.ac.jp>
2023-06-30 09:29:55 +0900

    地面のコード修正

--- MITI/Game/G_Ground.cpp
+++ MITI/Game/G_Ground.cpp
@@ -1,32 +1,49 @@
 #include "stdafx.h"
 #include "G_Ground.h"
 #include "Number_Storage.h"
-bool G_Ground::Start()
+G_Ground::G_Ground()
+{
+	InitModel();
+	InitPhysicsStaticObject();
+}
+
+void G_Ground::InitModel()
 {
 	for (int Y = 0; Y < 10; Y++)
 	{
 		for (int X = 0; X < 10; X++)
 		{
-			Ground[Y][X].Init("Assets/modelData/ground1.tkm", Light);
-			Ground_PSO[Y][X].CreateFromModel(Ground[Y][X].GetModel(), Ground[Y][X].GetModel().GetWorldMatrix());
-			Ground_PSO[Y][X].SetPosition({Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z});
-			Ground[Y][X].SetPosition({Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z});
-			Ground_On[Y][X] = false;
+			GroundModel[Y][X].Init("Assets/modelData/ground1.tkm", GroundLight);
+			GroundModel[Y][X].SetPosition({ Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z });
+			GroundOn[Y][X] = false;
 		}
 	}
-	return true;
+}
+void G_Ground::InitPhysicsStaticObject()
+{
+	for (int Y = 0; Y < 10; Y++)
+	{
+		for (int X = 0; X < 10; X++)
+		{
+			GroundPhysicsStaticObject[Y][X].CreateFromModel(GroundModel[Y][X].GetModel(), GroundModel[Y][X].GetModel().GetWorldMatrix());
+			GroundPhysicsStaticObject[Y][X].SetPosition({ Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z });
+		}
+	}
 }
 
-void G_Ground::Map_On(int Y, int X)
+void G_Ground::GroundOnTrue(int Y, int X)
 {
-	Ground_On[Y][X] = true;
+	GroundOn[Y][X] = true;
 }
-
-void G_Ground::Map_SetPosition(int Y, int X, Vector3 Position)
+void G_Ground::GroundSetPosition(int Y, int X, Vector3 Position)
 {
-	Ground_Position[Y][X].x = Position.x;
-	Ground_Position[Y][X].y = Position.y - 50.0f;
-	Ground_Position[Y][X].z = Position.z;
+	GroundPosition[Y][X].x = Position.x;
+	GroundPosition[Y][X].y = Position.y - 50.0f;
+	GroundPosition[Y][X].z = Position.z;
+
+	GroundSparePosition[Y][X].x = Position.x;
+	GroundSparePosition[Y][X].y = Position.y - 50.0f;
+	GroundSparePosition[Y][X].z = Position.z;
 }
 
 void G_Ground::Update()
@@ -35,36 +52,35 @@ void G_Ground::Update()
 	{
 		for (int X = 0; X < 10; X++)
 		{
-			if (Ground_On[Y][X] == true)
+			if (GroundOn[Y][X] == true)
 			{
-				if (Ground_Position[Y][X].x == NON)
+				if (GroundPosition[Y][X].x == NON || GroundPosition[Y][X].z == NON)
 				{
-					Ground[Y][X].SetPosition(Ground_SparePosition[Y][X]);
-					Ground_PSO[Y][X].SetPosition(Ground_SparePosition[Y][X]);
-					Ground[Y][X].Update();
+					GroundModel[Y][X].SetPosition(GroundSparePosition[Y][X]);
+					GroundPhysicsStaticObject[Y][X].SetPosition(GroundSparePosition[Y][X]);
+					GroundModel[Y][X].Update();
 				}else {
-					Ground[Y][X].SetPosition(Ground_Position[Y][X]);
-					Ground_PSO[Y][X].SetPosition(Ground_Position[Y][X]);
-					Ground[Y][X].Update();
+					GroundModel[Y][X].SetPosition(GroundPosition[Y][X]);
+					GroundPhysicsStaticObject[Y][X].SetPosition(GroundPosition[Y][X]);
+					GroundModel[Y][X].Update();
 				}
 			}else {
-					Ground[Y][X].SetPosition({Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z});
-					Ground_PSO[Y][X].SetPosition({Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z});
-					Ground[Y][X].Update();
+					GroundModel[Y][X].SetPosition({Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z});
+					GroundPhysicsStaticObject[Y][X].SetPosition({Grid_ExemptPosition_X,Grid_ExemptPosition_Y,Grid_ExemptPosition_Z});
+					GroundModel[Y][X].Update();
 			}
 		}
 	}
 }
-
 void G_Ground::Render(RenderContext& rc)
 {
 	for (int Y = 0; Y < 10; Y++)
 	{
 		for (int X = 0; X < 10; X++)
 		{
-			if (Ground_On[Y][X] == true)
+			if (GroundOn[Y][X] == true)
 			{
-				Ground[Y][X].Draw(rc);
+				GroundModel[Y][X].Draw(rc);
 			}
 		}
 	}
--- MITI/Game/G_Ground.h
+++ MITI/Game/G_Ground.h
@@ -2,17 +2,22 @@
 class G_Ground : public IGameObject
 {
 public:
-	bool Start();
-	void Map_On(int Y, int X);
-	void Map_SetPosition(int Y, int X, Vector3 Position);
+	G_Ground();
+
+	void InitModel();
+	void InitPhysicsStaticObject();
+
+	void GroundOnTrue(int Y, int X);
+	void GroundSetPosition(int Y, int X, Vector3 Position);
+
 	void Update();
 	void Render(RenderContext& rc);
 private:
-	ModelRender Ground[10][10];
-	Vector3 Ground_Position[10][10];
-	Vector3 Ground_SparePosition[10][10];
-	PhysicsStaticObjectPos Ground_PSO[10][10];
-	bool Ground_On[10][10];
-	AllLight Light;
+	ModelRender GroundModel[10][10];
+	Vector3 GroundPosition[10][10];
+	Vector3 GroundSparePosition[10][10];
+	PhysicsStaticObjectPos GroundPhysicsStaticObject[10][10];
+	AllLight GroundLight;
+	bool GroundOn[10][10];
 };
 
